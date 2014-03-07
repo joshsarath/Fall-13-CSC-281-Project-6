62a04fc2739f6d7a2691b5739cbb234bb1392d14
joshsarath <joshua.sarath@gmail.com>
2014-03-06 23:50:20 -0500

    These are the files

---
+++ project 6.2/project 6.2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  project 6.2
+//
+//  Created by Josh Sarath on 11/4/13.
+//  Copyright (c) 2013 Josh Sarath. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
